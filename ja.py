45258c8f75465c6474df55a949fb797d38dc6b8f
lightsing <light.tsing@gmail.com>
2018-03-11 15:04:38 +0800

    make template

--- ja.py
+++ ja.py
@@ -25,8 +25,21 @@ def index():
 @app.route('/video/av<int:vid>/')
 def video(vid):
     return render_template('video.html', search={
-                               'token': '123456',
-                               'placeholder': '加油哦'
+                                'token': '123456',
+                                'placeholder': '加油哦'
+                           }, video={
+                                'name': '标准日本语初级上册 {0:02}'.format(vid - 10000),
+                                'time': '2018-03-11 14:54',
+                                'play': 233,
+                                'danmu': 233,
+                                'url': '/static/video/01.mp4',
+                                'danmu_url': '/static/danmu/01.xml',
+                                'up': {
+                                    'name': 'Nihongo',
+                                    'bio': '君は日本語が本当に上手',
+                                    'uploads': 24,
+                                    'followers': 1123
+                                }
                            })
 
 
